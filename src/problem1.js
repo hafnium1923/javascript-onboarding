7645ef6a0edf772fcafc6d905113f23c80f54aed
hafnium1923 <hafnium1923@gmail.com>
2022-12-06 18:40:50 +0900

    feat 1번 문제

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,38 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(isWrongPage(pobi[0],pobi[1]) || isWrongPage(crong[0],crong[1]))
+    return -1;
+  if(whichMoreBig(pobi[0],pobi[1]) > whichMoreBig(crong[0], crong[1])) 
+    return 1;
+  else if(whichMoreBig(pobi[0],pobi[1]) > whichMoreBig(crong[0], crong[1]))
+    return 2;
+  else return 0;
 }
 
+function isWrongPage(left, right) {
+  if(left + 1 != right) return 1;
+  if(left == 1 || right == 400) return 1;
+}
+function whichMoreBig(left, right) {
+  const leftBig = plusAll(left) > multAll(left) ? plusAll(left) : multAll(left);
+  const rightBig = plusAll(right) > multAll(right) ? plusAll(right) : multAll(right);
+  return leftBig > rightBig ? leftBig : rightBig
+}
+
+function plusAll(num) {
+  let plus = 0;
+  while(num != 0) {
+    plus += parseInt(num % 10);
+    num = parseInt(num/10);
+  }
+  return plus;
+}
+
+function multAll(num) {
+  let mult = 1;
+  while(num != 0) {
+    mult *= parseInt(num % 10);
+    num = parseInt(num/10);
+  }
+  return mult;
+}
 module.exports = problem1;
