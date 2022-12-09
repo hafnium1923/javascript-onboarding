fd1900b426b3c255c9386e60533bff81424326a0
hafnium1923 <hafnium1923@gmail.com>
2022-12-09 16:02:18 +0900

    feat 3번 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
+const CONTAINER = [0,0,0,1,0,0,1,0,0,1];
+
 function problem3(number) {
   var answer;
+  isRightNum(number);
+  answer = countClap(number);
   return answer;
 }
 
+function isRightNum(number) {
+  if(number < 1 || number > 10000) throw '숫자 범위 오류';
+}
+
+function countClap(num) {
+  let count = 0;
+  for(let i = num; i > 0; i--) {
+    let number = i;
+    while(number > 0) {
+      count += CONTAINER[number%10];
+      number = parseInt(number/10);
+    }
+  }
+  return count;
+}
 module.exports = problem3;
